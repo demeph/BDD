bd96ec908c26a1e48fc7cad4c04cc759f46449b2
Demetre PHALAVANDISHVILI <demmetre@rocketmail.com>
2017-02-15 13:37:42 +0100

    modif

--- TP3/scriptCAAq2.sql
+++ TP3/scriptCAAq2.sql
@@ -1,8 +1,18 @@
 CREATE OR REPLACE PROCEDURE modifNoteCom(idc in number,refe in varchar2) IS
 	eval avis.note%type :=  &eval;
 	comment avis.commentaire%type := '&comment';
+	nbLigne number;
+	pas_droit exception;
 BEGIN
-	UPDATE avis set note = eval where idcl = idc and refl = refe;
-	UPDATE avis set commentaire = comment where idcl = idc and refl = refe;
+   Select count(*) into nbLigne from avis where idcl = idc and refl = refe;
+   if nbLigne is null then
+   	raise pas_droit;
+  	else 
+		UPDATE avis set note = eval where idcl = idc and refl = refe;
+		UPDATE avis set commentaire = comment where idcl = idc and refl = refe;
+	end if;
+exception
+	when pas_droit then
+		DBMS_OUTPUT.PUT_LINE('Il faut avoir un avis, avant de modifier l''avis');	
 END;
 /
